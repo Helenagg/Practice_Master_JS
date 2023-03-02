99121d089f4b62fc7b62e1efb6e765bf1de67b2e
Helenagg <hgonzalezg18@gmail.com>
2023-03-02 16:48:42 +0000

    59

--- exercises/059-removeFromBackOfNew/app.js
+++ exercises/059-removeFromBackOfNew/app.js
@@ -1,4 +1,8 @@
 function removeFromBackOfNew(arr) {
   // your code here
-  
+  let result = arr.filter((element, index) => index != arr.length-1);
+  return result; 
 }
+
+let output = removeFromBackOfNew([1, 2, 3, 4, 5]);
+console.log(output);
