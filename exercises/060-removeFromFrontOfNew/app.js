7683cfd51d9ef3da64e940a6b5f91e97cac8eafe
Helenagg <hgonzalezg18@gmail.com>
2023-03-02 18:47:41 +0000

    61 completed

--- exercises/060-removeFromFrontOfNew/app.js
+++ exercises/060-removeFromFrontOfNew/app.js
@@ -1,4 +1,8 @@
 function removeFromFrontOfNew(arr) {
     // your code here
-    
+    let result = arr.filter((element, index) => index !== 0);
+    return result;
 }
+
+let output = removeFromFrontOfNew([1, 2, 3, 4, 5]);
+console.log(output);
--- exercises/061-countCharacter/app.js
+++ exercises/061-countCharacter/app.js
@@ -1,4 +1,12 @@
 function countCharacter(str, char) {
     // your code here
-    
+    let count = 0;
+    for (let i=0; i< str.length; i++) {
+        if (str[i] === char) count = count+1;
+    }
+    return count;
 }
+
+let output = countCharacter('I am a hacker', 'a');
+console.log(output);
+
