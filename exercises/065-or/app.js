81e94cf4e88e5af6b0d089d5bb1d55357dc5d41c
Helenagg <hgonzalezg18@gmail.com>
2023-03-03 16:39:26 +0000

    66 completed

--- exercises/065-or/app.js
+++ exercises/065-or/app.js
@@ -1,4 +1,9 @@
 function or(expression1, expression2) {
   // your code here
-  
+  if (expression1 !== expression2) return true;
+  else if (expression1 && expression2) return true;
+  else return false;
 }
+
+let output = or(false, false);
+console.log(output);
