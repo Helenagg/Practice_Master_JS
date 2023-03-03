f67a2b6793db865c4c425f2a778b1e134dac004d
Helenagg <hgonzalezg18@gmail.com>
2023-03-03 12:18:56 +0000

    64 completed

--- exercises/062-getAllLetters/app.js
+++ exercises/062-getAllLetters/app.js
@@ -1,4 +1,8 @@
 function getAllLetters(str) {
     // your code here
-    
+    let arr = str.split("");
+    return arr;
 }
+
+let output = getAllLetters('helena');
+console.log(output);
--- exercises/063-getAllWords/app.js
+++ exercises/063-getAllWords/app.js
@@ -1,4 +1,8 @@
 function getAllWords(str) {
     // your code here
-    
+    let arr = str.split(" ");
+    return arr;
 }
+
+let output = getAllWords('Hoy es un dia precioso')
+console.log(output);
